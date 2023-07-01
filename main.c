c92bafe727f294701242ca6134f3644e805e5056
thaikum <fredthaiku@gmail.com>
2023-07-01 15:41:14 -0700

    using mpz

--- main.c
+++ main.c
@@ -1,27 +1,43 @@
+#include <gmp.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	unsigned long long int num, head, median;
+	mpz_t num, head, median, rem;
+	mpz_init(num);
+	mpz_init(head);
+	mpz_init(median);
+	mpz_init(rem);
 	char *num_buf = NULL, *stop;
 	size_t n = 0;
 
-	FILE *fp = fopen(argv[1],  "r");
+	FILE *fp = fopen(argv[1], "r");
 
 	while (getline(&num_buf, &n, fp) != -1)
 	{
-		num = strtoull(num_buf, &stop, 10);
-		head = num % 2 == 0? 2 : 3;
-		median = num / 2 + 1;
+		mpz_set_str(num, num_buf, 10);
+		mpz_div_ui(median, num, 2);
+		mpz_add_ui(median, median, 1);
+		if (mpz_mod_ui(head, num, 2) == 0)
+			mpz_set_ui(head, 2);
+		else
+			mpz_set_ui(head, 3);
 
-		for (; head < median; head += 2)
-			if (num % head == 0)
+		for (; mpz_cmp(head, median) < 0; mpz_add_ui(head, head, 2))
+		{
+			mpz_mod(rem, num, head);
+			if (mpz_cmp_ui(rem, 0) == 0)
 			{
-				printf("%llu=%llu*%llu\n", num, num / head, head);
+				mpz_div(rem, num, head);
+				gmp_printf("%Zd=%Zd*%Zd\n", num, rem, head);
 				break;
 			}
+		}
 	}
 	fclose(fp);
+	mpz_clear(num);
+	mpz_clear(median);
+	mpz_clear(head);
+	mpz_clear(rem);
 	return (0);
 }
